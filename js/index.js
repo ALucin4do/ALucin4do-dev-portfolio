81229440e347d29e27f678cf36236c5edff98f95
Lucino Campos <lucinogabriel1510@gmail.com>
2025-03-15 19:17:40 -0300

    ✨ feat: make the  navigation chooses dynamically which section to show

--- js/index.js
+++ js/index.js
@@ -1,4 +1,42 @@
 
+const buttonHome = document.getElementById('btn-home');
+const buttonAbout = document.getElementById('btn-about');
+const buttonSkills = document.getElementById('btn-skills');
+const buttonProjects = document.getElementById('btn-projects');
+const buttonExperience = document.getElementById('btn-experience');
+
+const home = document.getElementById('home');
+const about = document.getElementById('about');
+const skills = document.getElementById('skills');
+const projects = document.getElementById('projects');
+const experience = document.getElementById('experience');
+
+const buttons = [buttonHome, buttonAbout, buttonSkills, buttonProjects, buttonExperience];
+const sections = [home, about, skills, projects, experience];
+
+buttons.forEach((button) => {
+    /* everytime one of the buttons is clicked then... */
+    button.addEventListener('click', () => {
+        /* set all sections to display none */
+        sections.forEach((section) => {
+            section.style.display = 'none';
+        });
+
+        /* chose a section of the same index to display flex */
+        const index = buttons.indexOf(button);
+        sections[index].style.display = 'flex';
+    });
+});
+
+
+
+
+
+
+
+
+
+
 
 /*  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Home ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 const greetings = document.getElementById('typed-animation-name');
