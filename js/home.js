a98a8c5a4f78f9b8557af4199358160e5538cfc3
Lucino Campos <lucinogabriel1510@gmail.com>
2025-03-17 11:55:07 -0300

    ✨ feat: new animations to home section

--- js/home.js
+++ js/home.js
@@ -14,19 +14,47 @@ function showHome() {
             <div class="social-networks-container">
                 <a href="https://www.linkedin.com/in/lucino-de-campos/" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-linkedin"></i></a>
                 <a href="https://github.com/ALucin4do" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-github"></i></a>
-                <a href="mailto:lucinogabriel1510@gmail.com" ><i class="fa-solid fa-envelope"></i></a>
+                <a href="mailto:lucinogabriel1510@gmail.com" id="email" ><i class="fa-solid fa-envelope"></i></a>
             </div>
         </section>
     `;
 
     main.innerHTML = homeContent;
 
-    /* make the typing animation to the greetings */
+    /* typing animation to the greetings */
     const greetings = document.getElementById('typed-animation-name');
 
     var typed = new Typed(greetings, {
-        strings: ['Hello, ^1000 I am <mark>Lucino De Campos</mark>'],
+        strings: ['Hello, ^500 I am <mark>Lucino De Campos</mark>'],
         typeSpeed: 100,
     });
+
+    /* fade-in animation to the role */
+    const role = document.getElementById('role');
+    role.style.opacity = 0;
+    setTimeout(() => {
+        role.style.opacity = 1;
+        role.style.transform = 'translateY(0px)'
+    }, 5000);
+
+    /* fade-in animation to the social networks container */
+    const socialNetworksContainer = document.querySelector('.social-networks-container');
+    socialNetworksContainer.style.opacity = 0;
+    setTimeout(() => {
+        socialNetworksContainer.style.opacity = 1;
+        socialNetworksContainer.style.transform = 'translateY(0px)'
+    }, 6000);
+
+
+
+
+    /* copy email  */
+    const email = document.getElementById('email');
+    email.addEventListener('click', () => {
+        navigator.clipboard.writeText('lucinogabriel1510@gmail.com');
+        alert('Email copied to clipboard!');
+    });
+
+
 }
 
